2ad38b916c4788d1c5d30028dcd056e27dc1b8c4
GrantWelling <115586241+GrantWelling@users.noreply.github.com>
2024-05-24 17:18:45 -0400

    Add files via upload
    Attempt at recreating Q5 of Tavernlight Games Technical Trial

---
+++ frigo.lua
@@ -0,0 +1,29 @@
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+
+FRIGO_AREA = {
+	{0, 0, 0, 0, 1, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 0, 0, 0, 0},
+	{0, 0, 1, 1, 1, 1, 1, 0, 0},
+	{0, 1, 1, 1, 1, 1, 1, 1, 0},
+	{1, 1, 1, 1, 2, 1, 1, 1, 1},
+	{0, 1, 1, 1, 1, 1, 1, 1, 0},
+	{0, 0, 1, 1, 1, 1, 1, 0, 0},
+	{0, 0, 0, 0, 1, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 0, 0, 0, 0}
+}
+
+combat:setArea(createCombatArea(FRIGO_AREA))
+
+function onGetFormulaValues(player, level, magicLevel)
+	local min = (level / 5) + (magicLevel * 5.5) + 25
+	local max = (level / 5) + (magicLevel * 11) + 50
+	return -min, -max
+end
+
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(creature, variant)
+	return combat:execute(creature, variant)
+end
